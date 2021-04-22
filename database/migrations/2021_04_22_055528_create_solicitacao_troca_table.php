4bcac08eb2a36a9d343539fd4897777120beedfd
wendellunny <wendelferreiraportugal@gmail.com>
2021-04-22 03:07:32 -0300

    First Commit - Troca Lampadas | Sistema Solicitação de Troca de Lampadas

---
+++ database/migrations/2021_04_22_055528_create_solicitacao_troca_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolicitacaoTrocaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitacao_troca', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_cidadao',70);
+            $table->string('rua',255);
+            $table->string('numero',4);
+            $table->string('bairro',30);
+            $table->string('complemento',4);
+            $table->string('status',15);
+            $table->string('codigo',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitacao_troca');
+    }
+}
